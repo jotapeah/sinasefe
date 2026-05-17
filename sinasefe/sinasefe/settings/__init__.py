017109d30ef4de9f671b24f0d7416e66377f874e
jotapeah <jotapeah@jotapeah.com>
2026-05-17 16:44:19 -0400

    go

--- sinasefe/sinasefe/settings/__init__.py
+++ sinasefe/sinasefe/settings/__init__.py
@@ -6,7 +6,7 @@ env = environ.Env()
 
 BASE_DIR = Path(__file__).parent.parent.parent
 ENV_PATH = BASE_DIR.parent.joinpath(".env")
-environ.Env.read_env(BASE_DIR.joinpath(".env"))
+environ.Env.read_env(ENV_PATH)
 
 DEBUG = env.bool("DJANGO_DEBUG")
 
